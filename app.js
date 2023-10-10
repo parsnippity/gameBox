034db837f5bbb6637c7199f293c1f4f6bb400f01
parsnippity <briannesiemsen@gmail.com>
2023-10-10 13:22:37 -0700

    created and planned

---
+++ app.js
@@ -0,0 +1,20 @@
+//connect 4!
+/*User login
+Scoreboard - Top 10
+Post Game Screen
+Points
+Play new Games and Quit Options at any time
+multiplayer -(Locally)*/
+
+//red and yellow
+//click the row you want, red
+//now you, yellow
+//no one wins!
+//yellow has won its sixth game!
+//and the number of games can be stored under the login so we can call them by names instead of yellow
+//Bree's (yellow's) turn!
+//Bree has won six games!
+//top 10 works with users number they won
+//do we need a database to store these? we must
+//when you click the quit button it takes you to the home page
+//home page has login
